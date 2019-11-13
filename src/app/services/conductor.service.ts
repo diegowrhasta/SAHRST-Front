28e7adb29fa0ccc920396a926039b3b624d31a21
Yawar Antonio Valeriano Salinas <yawarvs@gmail.com>
2019-11-13 14:35:27 -0400

    Prueba commit Git parte 3

--- src/app/services/conductor.service.ts
+++ src/app/services/conductor.service.ts
@@ -3,66 +3,45 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from './global';
 import { Conductor } from '../models/conductor';
-import {ActivatedRoute, Router} from '@angular/router';
-
 
 @Injectable()
 export class ConductorService {
-
   public url: string;
-
-  //headers = new Headers();
-
   constructor(
-    private router: Router,
     public _http: HttpClient
-    ) { 
+    ) {
     this.url = GLOBAL.url;
-    //this.headers.append("Content-type","application/json");
-    //this.headers.append("Authorization","Bearer "+localStorage.getItem("access_token"));
   }
 
-  pruebas(){
-    return "Hola Mundo!!";
+  pruebas() {
+    return 'Hola Mundo!!';
   }
 
-  agregar(conductor): Observable<any>{
-    let json = JSON.stringify(conductor);
-    let params = 'json='+json;
-    let headers = new HttpHeaders().set('Authorization','Bearer '+localStorage.getItem('access_token')).set('Content-Type', 'application/json');
-
-    //let headers = new HttpHeaders().set('Content-Type', 'application/json');
-
-    return this._http.post(this.url+'Conductor', json, {headers: headers});
+  agregar(conductor): Observable<any> {
+    const json = JSON.stringify(conductor);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token')).set('Content-Type', 'application/json');
+    return this._http.post(this.url + 'Conductor', json, {headers: headers});
   }
 
-  getConductores():Observable<any>{
-    //if(localStorage.getItem('access_token')){
-      let headers = new HttpHeaders().set('Authorization','Bearer '+localStorage.getItem('access_token')).set('Content-Type', 'application/json');
-      return this._http.get(this.url+'Conductor', {headers: headers});  
-    //}
-    //console.log("no logueado");
-    //this.router.navigate(['login']).then();
+  getConductores(): Observable<any> {
+      const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token')).set('Content-Type', 'application/json');
+      return this._http.get(this.url + 'Conductor', {headers: headers});
   }
 
-  getConductor(id): Observable<any>{
-    let headers = new HttpHeaders().set('Authorization','Bearer '+localStorage.getItem('access_token')).set('Content-Type', 'application/json');
+  getConductor(id): Observable<any>  {
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token')).set('Content-Type', 'application/json');
     return this._http.get(this.url + 'Conductor/' + id, {headers: headers});
   }
 
-  update(conductor,id): Observable<any>{
-    let json = JSON.stringify(conductor);
-    let params = 'json='+json;
-    let headers = new HttpHeaders().set('Authorization','Bearer '+localStorage.getItem('access_token')).set('Content-Type', 'application/json');
-
-    //let headers = new HttpHeaders().set('Content-Type', 'application/json');
-
-    return this._http.put(this.url+'Conductor/' + id, json, {headers: headers});
+  update(conductor, id): Observable<any> {
+    const json = JSON.stringify(conductor);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token')).set('Content-Type', 'application/json');
+    return this._http.put(this.url + 'Conductor/' + id, json, {headers: headers});
   }
 
-  delete(id): Observable<any>{
-    let headers = new HttpHeaders().set('Authorization','Bearer '+localStorage.getItem('access_token')).set('Content-Type', 'application/json');
-    return this._http.delete(this.url+'Conductor/' + id, {headers: headers});
+  delete(id): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token')).set('Content-Type', 'application/json');
+    return this._http.delete(this.url + 'Conductor/' + id, {headers: headers});
   }
-  
+
 }
